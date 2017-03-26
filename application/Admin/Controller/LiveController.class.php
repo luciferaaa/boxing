6013c2511faca5d9e381de7b03c65a72d49bfd50
樊冬 <cg-man@localhost.localdomain>
2017-03-26 17:05:13 +0800

    直播的录入和修改的接口

--- application/Admin/Controller/LiveController.class.php
+++ application/Admin/Controller/LiveController.class.php
@@ -30,10 +30,65 @@ class LiveController extends AdminbaseController {
 		$this->display();
 	}
 
+	public function addPort() {
+		if(IS_POST) {
+			$data = [
+				'name' => $_POST['name'],
+				'leaves' => $_POST['leaves'],
+				'rounds' => $_POST['rounds'],
+				'player_red_id' => $_POST['player_red_id'],
+				'player_blue_id' => $_POST['player_blue_id'],
+				'image' => $_POST['image'],
+				'link' => $_POST['link'],
+				'player_red_base_num' => $_POST['player_red_base_num'],
+				'player_blue_base_num' => $_POST['player_blue_base_num'],
+				'money' => $_POST['money'],
+				'watcher_base_num' => $_POST['watcher_base_num'],
+				'place' => $_POST['place'],
+				'date' => $_POST['date']
+			];
+			$r = $this->live_model->where(['id'=>$data['id']])->add($data);
+			if($r !== false) {
+				$this->success('添加成功');
+			} else {
+				$this->error('添加失败');
+			}
+		}
+	}
+
 	public function edit() {
 		$id = $_GET['id'];
 		$data = $this->live_model->where(['id'=>$id])->select()[0];
 		$this->assign('data', $data);
 		$this->display();
 	}
+
+	public function editPort() {
+		if(IS_POST) {
+			$data = [
+				'id' => $_POST['id'],
+				'name' => $_POST['name'],
+				'leaves' => $_POST['leaves'],
+				'rounds' => $_POST['rounds'],
+				'player_red_id' => $_POST['player_red_id'],
+				'player_blue_id' => $_POST['player_blue_id'],
+				'image' => $_POST['image'],
+				'link' => $_POST['link'],
+				'player_red_base_num' => $_POST['player_red_base_num'],
+				'player_blue_base_num' => $_POST['player_blue_base_num'],
+				'money' => $_POST['money'],
+				'watcher_base_num' => $_POST['watcher_base_num'],
+				'place' => $_POST['place'],
+				'date' => $_POST['date']
+			];
+			$r = $this->live_model->where(['id'=>$data['id']])->save($data);
+			if($r !== false) {
+				$this->success('修改成功');
+			}else{
+				$this->error('修改失败');
+			}
+		}
+	}
+
+
 }
