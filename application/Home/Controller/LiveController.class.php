4f9a5c588329b189efda49ed6588d9fdb23a312c
cg-man <cg-man@outlook.com>
2017-03-27 16:22:11 +0800

    增加了前台页面的数据传入和前台获取players信息的接口

--- application/Home/Controller/LiveController.class.php
+++ application/Home/Controller/LiveController.class.php
@@ -2,11 +2,26 @@
 namespace Home\Controller;
 use Common\Controller\MobliebaseController;
 class LiveController extends MobliebaseController{
+
+	protected $players;
+	protected $live_list;
+	
 	function _initialize() {
+		$this->players = M('players');
+		$this->live_list = M('live_list');
 		parent::_initialize();
 	}
-	// 视频列表页面
+	// 直播详情页面
 	public function live() {
+		$id = $_GET['id'];
+		$data = $this->live_list->where(array('id'=>$id))->select()[0];
+		$this->assign('data', $data);
 		$this->display();
+	}
+
+	public function playersPort() {
+		$id = $_GET['id'];
+		$data = $this->players->where(array('id'=>$id))->select()[0];
+		$this->echo(json_encode($data));
 	}
 }
